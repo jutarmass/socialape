110361ff532f2c991d04a3680f006fe99530a986
jutarmass <jkhongjang@gmail.com>
2020-07-12 10:41:15 +0700

    new

--- functions/util/config.js
+++ functions/util/config.js
@@ -1,8 +1,10 @@
 module.exports = {
-  apiKey: 'AIzaSyBGE61bBXSyRfOccvujvR8VjWfB1BjW-Og',
-  authDomain: 'socialape-d081e.firebaseapp.com',
-  databaseURL: 'https://socialape-d081e.firebaseio.com',
-  projectId: 'socialape-d081e',
-  storageBucket: 'socialape-d081e.appspot.com',
-  messagingSenderId: '354040522108'
+  apiKey: "AIzaSyDNtA6DwTe5fx793ffucU447VjqhBc8Its",
+  authDomain: "socialape-34e0f.firebaseapp.com",
+  databaseURL: "https://socialape-34e0f.firebaseio.com",
+  projectId: "socialape-34e0f",
+  storageBucket: "socialape-34e0f.appspot.com",
+  messagingSenderId: "660222926818",
+  appId: "1:660222926818:web:f7667e345ee03c6eefe3bb",
+  measurementId: "G-HHDLQXS34M"
 };
