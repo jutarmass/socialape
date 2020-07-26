89da0c6ba60d4fe3cd63fa766d05130fd6c2cfc0
jutarmass <jkhongjang@gmail.com>
2020-07-26 21:38:46 +0700

    didmount

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,35 +1,32 @@
-import React, { Component } from 'react'
-import axios from 'axios';
-import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
-class home extends Component {
-  state = {
-    screams: null
-  }
-  componentDidMount() {
-    axios.get('/screams')
-    .then(res => {
-      console.log(res);
-      this.setState({
-        screams: res.data
-      });
-    })
-    .catch(err => console.log(err));
-  }
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
 
-  render() {
-    return (
-      <Grid container spacing={16}>
-        <Grid item sm={8} xs={12}>
-          <p>content...</p>
-          
-        </Grid>
-        <Grid item sm={4} xs={12}>
-          <p>profile...</p>
-        </Grid>
+function Home(props) {
+  const [screams, setScreams] = useState(null);
+  useEffect(() => {
+    axios
+      .get("/screams")
+      .then((res) => {
+        console.log(res.data);
+        setScreams({
+          screams: res.data,
+        });
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  return (
+    <Grid container spacing={16}>
+      <Grid item sm={8} xs={12}>
+        <p>cont</p>
       </Grid>
-    )
-  }
+      <Grid item sm={4} xs={12}>
+        <p>profile...</p>
+      </Grid>
+    </Grid>
+  );
 }
 
-export default home
+export default Home;
